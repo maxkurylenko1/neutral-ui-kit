c2908c41a0a30b9b0a5c5e0e2eed16a4d1af8ef0
Max <maxkurylenko1@gmail.com>
2025-10-30 12:12:53 +0100

    feat: add documentation for all components

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
