838fbaf7cac3cb72b17e7cb4651d4a37694891d2
Max <maxkurylenko1@gmail.com>
2025-11-02 15:13:14 +0100

    refactor: update ESLint configuration to use FlatCompat and improve module imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,25 @@
-import storybook from "eslint-plugin-storybook";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-]);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "node_modules/**",
+      "storybook-static/**",
+    ],
+  },
+];
 
 export default eslintConfig;
